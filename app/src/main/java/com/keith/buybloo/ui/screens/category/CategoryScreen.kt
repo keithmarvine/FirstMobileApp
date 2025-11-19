9dfed902356356bfa2870409e5a2c61e42f9b020
keithmarvine <mwegakeith91@gmail.com>
2025-11-19 10:36:40 +0300

    updates

--- app/src/main/java/com/keith/buybloo/ui/screens/category/CategoryScreen.kt
+++ app/src/main/java/com/keith/buybloo/ui/screens/category/CategoryScreen.kt
@@ -1,2 +1,63 @@
 package com.keith.buybloo.ui.screens.category
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun CategoryScreen(){
+
+    Column(
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        // TopApp Bar
+
+        TopAppBar(
+            title = { Text(text = "Product Categories") },
+            colors = TopAppBarDefaults.topAppBarColors(
+                containerColor = Color.Blue,
+                titleContentColor = Color.White,
+                navigationIconContentColor = Color.White
+
+            ),
+            navigationIcon = {
+                IconButton(onClick = {}) {
+                    Icon(imageVector = Icons.Default.Menu, contentDescription = "Menu")
+                }
+            }
+        )
+
+
+
+        //End of TopApp Bar
+
+
+
+
+
+
+    }
+
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun CategoryScreenPreview(){
+
+    CategoryScreen()
+
+}
